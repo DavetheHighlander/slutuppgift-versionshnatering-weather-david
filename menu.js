1b1766f061471213d306db9d55312678232b1503
Jihad Sayed <david.vakhayev@outlook.com>
2024-05-09 22:55:15 +0200

    sfd

---
+++ menu.js
@@ -0,0 +1,52 @@
+function openSidebar() {
+    document.getElementById("sidebar").style.width = "250px";
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+}
+document.addEventListener("DOMContentLoaded", function() {
+    loadPage("home.html", "page-container");
+    loadScript("weather.js");
+
+});
+
+document.getElementById("contactLink").addEventListener("click", function(event) {
+    event.preventDefault();
+    loadPage("contact.html", "page-container");
+});
+
+document.getElementById("homeLink").addEventListener("click", function(event) {
+    event.preventDefault();
+    loadPage("home.html", "page-container");
+    loadScript("weather.js");
+
+});
+document.getElementById("contactLinkMobile").addEventListener("click", function(event) {
+    event.preventDefault();
+    loadPage("contact.html", "page-container");
+    closeSidebar()
+});
+
+document.getElementById("homeLinkMobile").addEventListener("click", function(event) {
+    event.preventDefault();
+    loadPage("home.html", "page-container");
+    loadScript("weather.js");
+    closeSidebar()
+});
+function loadPage(page, container) {
+    // Use AJAX or fetch to load the content of the specified page
+    // Example using fetch:
+    fetch(page)
+        .then(response => response.text())
+        .then(html => {
+            // Replace the content of the container with the loaded page content
+            document.getElementById(container).innerHTML = html;
+        })
+        .catch(error => console.error("Error loading page:", error));
+}
+function loadScript(scriptUrl) {
+    const script = document.createElement('script');
+    script.src = scriptUrl;
+    document.head.appendChild(script);
+}
---
+++ weather.js
@@ -0,0 +1,45 @@
+document.getElementById('weatherForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const city = document.getElementById('cityInput').value;
+    getWeatherData(city);
+});
+
+
+function getWeatherData(city) {
+    const apiKey = '31fb328bf7f84855ae3185427241602';
+    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+        // https://api.openweathermap.org/data/2.5/weather?q=malmö&appid=31fb328bf7f84855ae3185427241602&units=metric
+        // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            displayWeatherInfo(data);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+            displayWeatherInfoError();
+        });
+}
+
+function displayWeatherInfo(data) {
+    const weatherInfoContainer = document.getElementById('weatherInfo');
+    weatherInfoContainer.innerHTML = `
+        <h2>${data.location.name}, ${data.location.country}</h2>
+        <p>Temperature: ${data.current.temp_c} &deg;C</p>
+        <p>Condition: ${data.current.condition.text}</p>
+        <img src="https:${data.current.condition.icon}" alt="Weather Icon">
+        <p>Wind Speed: ${data.current.wind_kph} km/h, Direction: ${data.current.wind_dir}</p>
+        <p>Pressure: ${data.current.pressure_mb} mb</p>
+        <p>Humidity: ${data.current.humidity}%</p>
+        <p>Cloudiness: ${data.current.cloud}%</p>
+        <p>Feels Like: ${data.current.feelslike_c} &deg;C</p>
+        <p>Visibility: ${data.current.vis_km} km</p>
+        <p>UV Index: ${data.current.uv}</p>
+    `;
+}
+
+function displayWeatherInfoError() {
+    const weatherInfoContainer = document.getElementById('weatherInfo');
+    weatherInfoContainer.innerHTML = '<p>Error fetching weather data. Please try again.</p>';
+}
